4c251e4bdffff589b46135e6d52ba735d72c08e8
Amy <amee_sk@yahoo.com>
2020-08-02 12:13:52 +0100

    Final form

--- specs/count_even_numbers_spec.rb
+++ specs/count_even_numbers_spec.rb
@@ -1,6 +1,13 @@
 require '../test_framework.rb'
 
 # Acceptance Criteria
-[1,2,3,4,5,6] => 3
-[0,1,2] => 2
-[1,3,5] => 0
+acceptance_criteria = [[[1,2,3,4,5,6], 3],
+ [[0,1,2], 2], 
+ [[1,3,5], 0]]
+acceptance_criteria.each do |test|
+# Set up, execute
+    actual = count_even_numbers(test[0])
+    expected = test[1]
+# Verify
+    puts assert_equals(actual, expected)
+end
